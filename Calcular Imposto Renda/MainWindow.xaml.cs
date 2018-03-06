47a54c3c6d26032a316ceb55febe1af574356fe7
ArielSimao <arielsimao@outlook.com>
2018-03-06 15:23:26 -0300

    Initial commit

---
+++ Calcular Imposto Renda/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calcular_Imposto_Renda
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, RoutedEventArgs e)
+        {
+            /*    salario            Desconto
+                 =< 1900                 0 %
+                1901 - 2800             10 %
+                 >= 2801                15 %
+            */
+        }
+    }
+}
